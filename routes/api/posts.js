7dc9be730cc588c7edb14e4ed2332f4777141b9c
newintenz <nikolamiletic3@gmail.com>
2020-08-21 12:38:05 +1000

    Updated posts.js
    Comments can be added and removed as well as Posts.... Last video in the back-end Node.js section... moving on to Bootstrap theme picking and React

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -10,7 +10,6 @@ const Profile = require("../../models/Profile");
 // Validation
 const validatePostInput = require("../../validation/post");
 
-
 // @route    GET api/posts/test
 // @desc     tests posts route
 // @access   public route
@@ -127,6 +126,65 @@ router.post("/unlike/:id", passport.authenticate("jwt", {session: false}), (req,
 
         }).catch(err => res.status(404).json({ postnotfound: "Post not found"}));
     })
+})
+
+
+
+// @route    POST api/posts/comment/:id
+// @desc     Comment on a post
+// @access   private
+router.post('/comment/:post_id', passport.authenticate("jwt" , {session: false}) , (req, res) => {
+
+    const  { errors , isValid } = validatePostInput(req.body);
+
+    // Check Validation 
+    if(!isValid){
+        // Check for any errors and send status 400 if any
+        return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.post_id)
+    .then( post => {
+
+        // Prepare an object containing comment data
+        const newComment = {
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+        }
+
+        // Add comment to array of comments
+        post.comments.unshift(newComment);
+
+        // save
+        post.save().then( post => res.json(post))
+        
+    }).catch(err => res.status(404).json({ postnotfound: "No post found" }));
+})
+
+
+
+// @route    DELETE api/posts/comment/:post_id/:comment_id
+// @desc     Delete a comment from a post
+// @access   private
+router.delete('/comment/:id/:comment_id', passport.authenticate("jwt" , {session: false}) , (req, res) => {
+
+    Post.findById(req.params.id).then(post => {
+        // Check if comment exists
+        if(post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0){
+            return res.status(404).json({ commentnotexists : "Comment doesn't exist"});
+        }
+
+        // If comment present get remove index 
+        const removeIndex = post.comments.map( item => item._id.toString()).indexOf(req.params.comment_id);
+        
+        // Splice remove index
+        post.comments.splice(removeIndex, 1);
+
+        // save
+        post.save().then( post => res.json(post));
+    }).catch(err => res.status(404).json({ postnotfound: 'No post found' }));
 })
 
 module.exports = router;
